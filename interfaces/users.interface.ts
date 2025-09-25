5b572f0ffa9a8654ad9dfb6b82d870a945b81488
rolaaagp <143853171+rolaaagp@users.noreply.github.com>
2025-09-24 23:21:02 -0300

    [feature][payload crear usuario y constante con id perfil ciudadano]

---
+++ interfaces/users.interface.ts
@@ -0,0 +1,11 @@
+export interface PayloadCreateUser {
+  perfiles_id: number;
+  run: string;
+  fullname: string;
+  telefono: string;
+  password: string;
+}
+
+export const PERFILES = {
+  CIUDADANO: 2,
+};
